193675f40131853d07b9204d70dca88b56faf38e
isacamilli <isadevprojects@gmail.com>
2024-10-18 21:05:37 -0300

    Adicionando nova pasta e arquivos

---
+++ Exercicios/Lista01/ex1.py
@@ -0,0 +1,15 @@
+class cerchio :
+    p = 3.14
+    def __init__(self) :
+        self.r = 0
+    def area(self) :
+        # p = 3.14
+        area = self.r**2 * self.p
+        return area
+    def circunferencia(self) :
+        circ = 2 * self.p * self.r 
+        return circ
+
+a = cerchio()
+a.r = 3
+print (a.area(), a.circunferencia())

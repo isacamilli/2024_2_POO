737794754f7ae64b888229a7a4b335ff12fcd80a
Isabella Camilli Meletani <20241014040028@ifrn.local>
2024-10-31 10:26:31 -0300

    att

---
+++ Exercicios/LIsta04/Ex1.py
@@ -0,0 +1,36 @@
+import randint from random
+
+class Bingo :
+    def __init__(self,numBolas,bolas) :
+        self.__numBolas = numBolas
+        self.__bolas = bolas
+    def __str__(self) :
+        return self.__bolas
+
+
+class UI :
+    @staticmethod
+    def menu() :
+        print ("Quer começar um novo jogo?")
+        print("1 - Sim, 0 - Não")
+        return int(input())
+
+    @staticmethod
+    def main() :
+        op = 1
+        jogo = None
+        while op == 1 :
+            op = UI.menu()
+            if op == 1 :
+                jogo = UI.novo_jogo()
+            
+            
+
+
+    @staticmethod
+    def novo_jogo() :
+        num_b = int(input("Insira a quantidade de bolas dessa rodada: "))
+        tuas_bolas = list(map(int,input("Insira os números sorteados por você: ").split()))
+        jogoCriado = Bingo(num_b,tuas_bolas)
+    
+    
